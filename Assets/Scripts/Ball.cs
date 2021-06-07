13e72bb3095e64f1eefa09cbcb4de30fdb6255ed
Harrison <harrisonhough4@gmail.com>
2021-06-07 10:53:16 +0300

    Removed unused function
    Also added more debug lines

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -16,11 +16,11 @@ public class Ball : MonoBehaviour {
     [SerializeField]
     private float xForceScale = 1f;
     [SerializeField]
-    private float yForceScale = 1f;
+    private float yForceScale = 0.85f;
     [SerializeField]
-    private float zForceScale = 1f;
+    private float zForceScale = 0.65f;
     [SerializeField]
-    private float power = 50f;
+    private float power = 20f;
 
     private Rigidbody rigidbodyComponent;
     public Rigidbody RigidbodyComponent => rigidbodyComponent;
@@ -42,6 +42,10 @@ public class Ball : MonoBehaviour {
 
     public void Shoot(Vector3 force)
     {
+        if (force.sqrMagnitude < 1)
+        {
+            return;
+        }
         hasScored = false;
         rigidbodyComponent.useGravity = true;
         Vector3 result = CalculateForce(force);
@@ -50,48 +54,21 @@ public class Ball : MonoBehaviour {
         GetComponent<BallRecycle>().StartRecycle();
     }
 
-    private void Calculate()
-    {
-        var rigid = GetComponent<Rigidbody>();
-        var test = Vector3.one;
-        Vector3 p = test;
- 
-        float gravity = Physics.gravity.magnitude;
-        // Selected angle in radians
-        float angle = 45 * Mathf.Deg2Rad;
- 
-        // Positions of this object and the target on the same plane
-        Vector3 planarTarget = new Vector3(p.x, 0, p.z);
-        Vector3 planarPostion = new Vector3(transform.position.x, 0, transform.position.z);
- 
-        // Planar distance between objects
-        float distance = Vector3.Distance(planarTarget, planarPostion);
-        // Distance along the y axis between objects
-        float yOffset = transform.position.y - p.y;
- 
-        float initialVelocity = (1 / Mathf.Cos(angle)) * Mathf.Sqrt((0.5f * gravity * Mathf.Pow(distance, 2)) / (distance * Mathf.Tan(angle) + yOffset));
- 
-        Vector3 velocity = new Vector3(0, initialVelocity * Mathf.Sin(angle), initialVelocity * Mathf.Cos(angle));
- 
-        // Rotate our velocity to match the direction between the two objects
-        float angleBetweenObjects = Vector3.Angle(Vector3.forward, planarTarget - planarPostion);
-        Vector3 finalVelocity = Quaternion.AngleAxis(angleBetweenObjects, Vector3.up) * velocity;
- 
-        // Fire!
-        //rigid.velocity = finalVelocity;
- 
-        // Alternative way:
-        rigid.AddForce(finalVelocity * rigid.mass, ForceMode.Impulse);
-    }
-
 
     private Vector3 CalculateForce(Vector3 force)
     {
+        var maxY = 5;
+        //force.y = Mathf.Clamp(force.y, 0, maxY);
+        var maxX = 1.5f;
+        //force.x = Mathf.Clamp(force.x, 0, maxX);
         Debug.Log($"force vector {force}");
-        return new Vector3(
+        var finalForce = new Vector3(
             force.x * xForceScale * power,
-            force.y * yForceScale * 0.75f * power,
-            force.y * zForceScale * 0.75f * power);
+            force.y * yForceScale * power,
+            force.y * zForceScale * power);
+        Debug.Log($"finalForce vector {finalForce}");
+
+        return finalForce;
     }
 
     public void DisableGravity()
